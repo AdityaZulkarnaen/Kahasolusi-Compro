63c86601c0ca607a079b0c8e69a100556af34a71
AdityaZulkarnaen <adityazulkarnaen083@gmail.com>
2025-12-06 18:13:30 +0700

    fix: minor change

--- src/lib/modules/Portfolio/Portfolio.svelte
+++ src/lib/modules/Portfolio/Portfolio.svelte
@@ -308,7 +308,7 @@
         <div class="max-w-7xl mx-auto text-center">
             <div class="mb-16">
                 <h1 class="text-4xl md:text-5xl font-medium font-family-sans text-gray-900 mb-4">
-                    Portfolio Proyek.
+                    Portfolio Proyek
                 </h1>
                 <div class="w-64 h-1 bg-[#176684] mx-auto mb-6"></div>
             </div>
